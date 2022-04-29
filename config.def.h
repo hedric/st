4ac43273eabf637ee243b879436fc53c57ea4b2a
Richard Hedlund <richard.hedlund@gmail.com>
2022-04-29 21:04:16 +0200

    font: switch to ubuntu mono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka-Regular:pixelsize=20:antialias=true:autohint=false";
+static char *font = "UbuntuMono:pixelsize=20:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
